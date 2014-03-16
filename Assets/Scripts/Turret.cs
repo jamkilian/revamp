60a006124c3eee4a10134d5edf85271d9efda55a
jamkilian <jamkilian@gmail.com>
2014-03-16 17:39:55 -0400

    Level and turret start

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Turret : MonoBehaviour {
-
+public class Turret : MonoBehaviour 
+{
+	/* 1) Turrets should only attack enemies in it's lane
+	 * 2) Turrets should attack closest enemy
+	 *
+	 * 
+	 * 
+	 */
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+	    Debug.DrawRay(this.gameObject.transform.position, this.gameObject.transform.forward, Color.cyan, 5f);
 	}
 }
