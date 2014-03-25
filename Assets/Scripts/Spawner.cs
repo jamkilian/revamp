a56f542bf47cadeb7eb56d5a24eb588808584a4a
jamkilian <jamkilian@gmail.com>
2014-03-25 19:46:58 -0400

    tabify

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -33,8 +33,8 @@ public class Spawner : MonoBehaviour
 			
 			//Spawning the enemy
 			_spawnedEnemy = Instantiate(enemyToSpawn, _t.position, Quaternion.identity) as GameObject;
-            EnemyScript es = _spawnedEnemy.GetComponent<EnemyScript>();
-            es.SetSpeed(baseEnemySpeed);
+			EnemyScript es = _spawnedEnemy.GetComponent<EnemyScript>();
+			es.SetSpeed(baseEnemySpeed);
 		}
 	}
 
