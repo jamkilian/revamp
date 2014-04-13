98221bc4dce73adb0158ea883805c45eb860a50a
jamkilian <jamkilian@gmail.com>
2014-04-13 14:11:27 -0400

    Now with bosses at every 7 enemies and end of wave!

--- Assets/Scripts/newWave.cs
+++ Assets/Scripts/newWave.cs
@@ -8,40 +8,54 @@ public class newWave : MonoBehaviour
     public GameObject EnemyPrefab;
     private Object BossPrefab;
     public int enemiesAmount;
+    public int bossAmount;
+    private int bossInterval = 7;
     public float spawnWait;
     public float startWait;
     public float waveWait;
     public float baseEnemySpeed = 3.5f;
     public static int waves;
+    private GameObject pos;
 
-    void Start ()
+    void Start()
     {
         BossPrefab = Resources.Load("BossEnemy");
-        StartCoroutine (SpawnWaves ());
+        StartCoroutine(SpawnWaves());
         SpawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
         waves = 1;
     }
 
-    IEnumerator SpawnWaves ()
+    IEnumerator SpawnWaves()
     {
-        yield return new WaitForSeconds (startWait);
+        yield return new WaitForSeconds(startWait);
         while (true)
         {
+            CreateWave();
             for (int i = 0; i < enemiesAmount; i++)
             {
-                GameObject pos = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(EnemyPrefab, pos.transform.position, pos.transform.rotation);
+                pos = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
+                if ((i % bossInterval) == 0 && i != 0)
+                    Instantiate(BossPrefab, pos.transform.position, Quaternion.identity);
+                else
+                    Instantiate(EnemyPrefab, pos.transform.position, Quaternion.identity);
+
+
                 EnemyScript es = EnemyPrefab.GetComponent<EnemyScript>();
                 es.SetSpeed(baseEnemySpeed);
-                yield return new WaitForSeconds (spawnWait);
+                yield return new WaitForSeconds(spawnWait);
             }
-            GameObject bossPos = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
-            Instantiate(BossPrefab, bossPos.transform.position, bossPos.transform.rotation);
-            yield return new WaitForSeconds (waveWait);
+            Instantiate(BossPrefab, pos.transform.position, pos.transform.rotation);
+            yield return new WaitForSeconds(waveWait);
             newWave.waves++;
         }
     }
+
+    private void CreateWave()
+    {
+        enemiesAmount = waves * 5;
+        bossAmount = (int)Mathf.Floor(enemiesAmount / bossInterval);
+    }
+
     void Update()
     {
         PlayerPrefs.SetInt("Waves", waves);
