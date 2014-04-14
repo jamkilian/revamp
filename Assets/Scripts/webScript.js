996fc0ca10627a5de2fb92b10915762191caff9a
natedog735 <nathan.walczak@aol.com>
2014-04-14 10:57:42 -0400

    working on enemy health gui

---
+++ Assets/Scripts/webScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () 
+{
+	var url = "http://nathanwalczak.net/revampDB/signIN.php?Data=nate";
+	var www:WWW = new WWW(url);
+
+	yield www;
+}
+
+function Update () 
+{
+
+}
